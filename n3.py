b12d60a9072b14f8a0f1ac95ff3db10cd1d1f298
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-12 15:44:23 +0530

    Create n3.py

---
+++ n3.py
@@ -0,0 +1,10 @@
+x=input()
+if((x>='a' and x<='z') or (x>='A' and X<='Z')):
+  x.lower()
+  if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+     print("Vowel")
+  else:
+     print("Consonant")
+else:
+    print("Special Characters")
+    
