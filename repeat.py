e0969e9635a7ecbab438888f3f3676f15bad38f3
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-13 16:13:48 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,8 +1,14 @@
 l=[]
+l1=[]
+s=""
 x=input()
 for i in range(0,x):
   l.append(input("Enter  a number"))
 for i in range(0,x):
   for j in range(i+1,x):
      if(l[i]==l[j]):
-        print(l[i],end=('')
+       # print(l[i],end=('')
+       l1=l[i]
+l1.sort()
+for i in l1:
+  s+=" "+str(i)
