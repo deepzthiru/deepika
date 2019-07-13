e5e2853c0268e44f2740d8feb01c42f2c9f2757d
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-13 16:16:45 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -2,13 +2,16 @@ l=[]
 l1=[]
 s=""
 x=input()
-for i in range(0,x):
+if(x>=1 and x<=100000):
+  for i in range(0,x):
   l.append(input("Enter  a number"))
-for i in range(0,x):
-  for j in range(i+1,x):
-     if(l[i]==l[j]):
+  for i in range(0,x):
+    for j in range(i+1,x):
+      if(l[i]==l[j]):
        # print(l[i],end=('')
        l1=l[i]
+      else:
+        print("unique")
 l1.sort()
 for i in l1:
-  s+=" "+str(i)
+   s+=" "+str(i)
