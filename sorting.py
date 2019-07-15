28e60320f1051ef530dc261d7237c236de459dc4
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-15 10:48:31 -0700

    Update and rename sort.py to sorting.py

--- sorting.py
+++ sorting.py
@@ -1,12 +1,12 @@
 n=int(input())
-elements=list(map(int,input().split()))
+num=list(map(int,input().split()))
 l1=[]
-for i in elements:
-    if elements.count(i)>1:
+for i in num:
+    if num.count(i)>1:
         if str(i) not in l1:
             l1.append(str(i))
 if len(l1)==0:
     print("unique")
 else:
-    elements.sort()
+    num.sort()
     print(" ".join(l1))
