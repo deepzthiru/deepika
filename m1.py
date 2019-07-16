8d6e4223c6d1fdfe936e0a2ad8682471d846611c
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-16 15:09:55 +0530

    Update m1.py

--- m1.py
+++ m1.py
@@ -4,8 +4,8 @@ l1=[]
 for i in range(0,len(l)):
   if i==l[i]:
     l1.append(l[i])
-  if len(l1)==0:
-     print("-1")
-  else:
-    for x in range(len(l1)):
-       print(l1[x],end=" ")
+if len(l1)==0:
+  print("-1")
+else:
+  for x in range(len(l1)):
+     print(l1[x],end=" ")
