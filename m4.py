5abb53d2a95156c226154dd0e0a5b432a99c6566
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-16 15:47:43 +0530

    Create m4.py

---
+++ m4.py
@@ -0,0 +1,12 @@
+x=int(input())
+a=input("")
+a=list(a.split(" "))
+l=[]
+for i in range(0,len(a)):
+   for j in range(i+1,len(a)):
+      if a[i]==a[j]:
+        l.append(a[i])
+if(len(l)==0):
+   print("uniqye")
+else:
+   print(l[o])
