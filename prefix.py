da118f570b108b6b299ed94c7060a7bf8ab70d5d
deepzthiru <52820087+deepzthiru@users.noreply.github.com>
2019-07-19 15:25:04 +0530

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,17 @@
+l1=int(input())
+k=[]
+for i in range(0,l1):
+   k.append(input())
+l1=len(k[0])
+s=""
+for i in range(0,l1):
+   c=k[0][i]
+   f=0
+   for j in range(0,l1):
+        if(c!=k[j][i]):
+           f=1
+   if(f==0):
+       s=s+c
+  else:
+      break
+print(s)
